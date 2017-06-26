9337adf3f2180d11b6c7adbf6a8d5f94aaf4842f
mpenna <tworker@onyx.ove.com>
2017-06-26 14:13:03 -0400

    Started to work on not allowing duplicate template names for a company

--- src/main/java/com/ofs/repository/TemplateRepository.java
+++ src/main/java/com/ofs/repository/TemplateRepository.java
@@ -1,7 +1,11 @@
 package com.ofs.repository;
 
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.error.TemporaryFailureException;
+import com.couchbase.client.java.query.ParameterizedN1qlQuery;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ofs.model.Template;
+import com.ofs.server.errors.ServiceUnavailableException;
 import com.ofs.server.repository.BaseCouchbaseRepository;
 import com.ofs.server.repository.ConnectionManager;
 import com.ofs.server.repository.OFSRepository;
@@ -9,7 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -25,5 +31,34 @@ public class TemplateRepository extends BaseCouchbaseRepository<Template>{
         log.info("Attempting to add template with id: {}", template.getId());
         add(template.getId().toString(), connectionManager.getBucket("template"), template);
         log.info("Template with id: {} has been added", template.getId());
+    }
+
+    public Optional<Template> getTemplateByName(String name, String companyHref) throws Exception {
+        if(name == null) {
+            return Optional.empty();
+        }
+
+        try {
+            ParameterizedN1qlQuery query = ParameterizedN1qlQuery.parameterized(
+                    generateGetByNameQuery(), generateGetByNameParameters(name, companyHref));
+            return queryForObjectByParameters(query, connectionManager.getBucket("template"), Template.class);
+        }
+        catch (NoSuchElementException e) {
+            log.info("No results returned for getTemplatebyName with name: {}", name);
+            return Optional.empty();
+        }
+        catch (TemporaryFailureException e) {
+            log.error("Temporary Failure with couchbase occured" , e);
+            throw new ServiceUnavailableException();
+        }
+    }
+
+    private String generateGetByNameQuery() {
+        return "SELECT `" + connectionManager.getBucket("template").name() + "`.* FROM `" + connectionManager.getBucket("template").name()
+                + "` where name = $name and companyHref = $companyHref";
+    }
+
+    private JsonObject generateGetByNameParameters(String name, String companyHref) {
+        return JsonObject.create().put("$name", name).put("$companyHref", companyHref);
     }
 }
---
+++ src/main/java/com/ofs/validations/template/ValidateTemplateNameDNE.java
@@ -0,0 +1,30 @@
+package com.ofs.validations.template;
+
+import com.ofs.model.Template;
+import com.ofs.repository.TemplateRepository;
+import com.ofs.server.form.update.ChangeSet;
+import com.ofs.server.model.OFSErrors;
+import com.ofs.validations.TemplateCreateValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class ValidateTemplateNameDNE implements TemplateCreateValidation {
+
+    @Autowired
+    private TemplateRepository templateRepository;
+
+    @Override
+    public void validate(Template template, OFSErrors errors) throws Exception {
+        Optional optionalTemplate = templateRepository.getTemplateByName(template.getName());
+
+        if(optionalTemplate.isPresent()) {
+            errors.rejectValue("template.name.exists", "template.name", "Invalid template name. Template already exists.");
+        }
+    }
+
+    @Override
+    public void validate(ChangeSet changeSet, Template template, OFSErrors errors) throws Exception {
+        validate(template, errors);
+    }
+}
