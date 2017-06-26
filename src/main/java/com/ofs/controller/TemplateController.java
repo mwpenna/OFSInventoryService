4d84eb6a5b1e9ad0ff4c1827c63bfff870362d12
mpenna <tworker@onyx.ove.com>
2017-06-26 10:11:49 -0400

    Added Cross Origin and Authentication

--- src/main/java/com/ofs/controller/TemplateController.java
+++ src/main/java/com/ofs/controller/TemplateController.java
@@ -5,8 +5,10 @@ import com.ofs.server.OFSController;
 import com.ofs.server.OFSServerId;
 import com.ofs.server.form.OFSServerForm;
 import com.ofs.server.form.ValidationSchema;
+import com.ofs.server.security.Authenticate;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +20,8 @@ public class TemplateController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ValidationSchema(value = "/template-create.json")
+    @Authenticate
+    @CrossOrigin(origins = "*")
     public ResponseEntity create(@OFSServerId URI id, OFSServerForm<Template> form) throws Exception{
         form.create(id);
         return ResponseEntity.created(id).build();
