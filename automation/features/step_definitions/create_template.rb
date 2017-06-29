14a312bcb068f343233cfde82cf9bd0b21f44cd0
mpenna <tworker@onyx.ove.com>
2017-06-29 11:42:40 -0400

    Completed all automation tests for create template endpoint

--- automation/features/step_definitions/create_template.rb
+++ automation/features/step_definitions/create_template.rb
@@ -1,5 +1,6 @@
 Given(/^A (.*?) user exists for a company$/) do |property|
-  jwtsubject = FactoryGirl.build(:jwtsubject, role: property)
+  @companyId = SecureRandom.uuid
+  jwtsubject = FactoryGirl.build(:jwtsubject, role: property, companyHref: "http://localhost:8080/company/id/" + @companyId)
   request = '{"status": 200, "message":'+ jwtsubject.to_json+'}'
   @service_client.post_to_url(@service_client.get_mock_base_uri + '/users/authenticate/status', request)
 end
@@ -22,6 +23,7 @@ Given(/^A ADMIN user and template exists for a company$/) do
   prop1 = FactoryGirl.build(:prop, name: 'color', required: true, type:'STRING')
   prop2 = FactoryGirl.build(:prop, name: 'size', required: true, type:'NUMBER')
   @template = FactoryGirl.build(:template, name: Faker::Name.name , props: [prop1, prop2])
+  @template.companyId = @companyId
   @service_client.post_to_url_with_auth("/inventory/template", @template.create_to_json, "Bearer "+ "123")
 end
 
@@ -36,8 +38,9 @@ end
 When(/^A request to create a template is received$/) do
   prop1 = FactoryGirl.build(:prop, name: 'color', required: true, type:'STRING')
   prop2 = FactoryGirl.build(:prop, name: 'size', required: true, type:'NUMBER')
-  template = FactoryGirl.build(:template, name: Faker::Name.name , props: [prop1, prop2])
-  @result = @service_client.post_to_url_with_auth("/inventory/template", template.create_to_json, "Bearer "+ "123")
+  @template = FactoryGirl.build(:template, name: Faker::Name.name , props: [prop1, prop2])
+  @result = @service_client.post_to_url_with_auth("/inventory/template", @template.create_to_json, "Bearer "+ "123")
+  @location = @result.headers['location']
 end
 
 When(/^A request to create a template is received with missing (.*?)$/) do |field|
@@ -74,8 +77,10 @@ end
 When(/^A request to create a template is received with a companyId$/) do
   prop1 = FactoryGirl.build(:prop, name: 'color', required: true, type:'STRING')
   prop2 = FactoryGirl.build(:prop, name: 'size', required: true, type:'NUMBER')
-  template = FactoryGirl.build(:template, name: Faker::Name.name , props: [prop1, prop2])
-  @result = @service_client.post_to_url_with_auth("/inventory/template", template.create_to_json, "Bearer "+ "123")
+  companyId = SecureRandom.uuid
+  @template = FactoryGirl.build(:template, name: Faker::Name.name , props: [prop1, prop2], companyId: companyId)
+  @result = @service_client.post_to_url_with_auth("/inventory/template", @template.create_to_json_with_company_id, "Bearer "+ "123")
+  @location = @result.headers['location']
 end
 
 Then(/^the response should have a status of (\d+)$/) do |response_code|
@@ -96,4 +101,58 @@ end
 
 And(/^I should see an error message indicating prop (.*?) missing$/) do |property|
   expect(@result["errors"][0]).to eql Errors.props_required_field_missing(property)
+end
+
+Then(/^I should the template was created$/) do
+  result = @service_client.get_by_url_with_auth(@location, 'Bearer 123')
+
+  expect(result["companyId"]).to eql @companyId
+  expect(result["name"]).to eql @template.name
+  expect(result["createdOn"]).to_not be_nil
+  expect(result["id"]).to_not be_nil
+  expect(result["href"]).to_not be_nil
+  expect(result["props"]).to_not be_nil
+
+  result["props"].each do |property|
+    prop = @template.props.detect{|u| u.name == property['name']}
+    expect(prop.name).to eql prop.name
+    expect(prop.required).to eql prop.required
+    expect(prop.type).to eql prop.type
+  end
+end
+
+Then(/^I should see the companyId is equal to the user companyId$/) do
+  result = @service_client.get_by_url_with_auth(@location, 'Bearer 123')
+
+  expect(result["companyId"]).to eql @companyId
+  expect(result["name"]).to eql @template.name
+  expect(result["createdOn"]).to_not be_nil
+  expect(result["id"]).to_not be_nil
+  expect(result["href"]).to_not be_nil
+  expect(result["props"]).to_not be_nil
+
+  result["props"].each do |property|
+    prop = @template.props.detect{|u| u.name == property['name']}
+    expect(prop.name).to eql prop.name
+    expect(prop.required).to eql prop.required
+    expect(prop.type).to eql prop.type
+  end
+end
+
+Then(/^I should see the companyId is equal to the requested companyId$/) do
+  result = @service_client.get_by_url_with_auth(@location, 'Bearer 123')
+
+  expect(result["companyId"]).to eql @template.companyId
+  expect(result["name"]).to eql @template.name
+  expect(result["createdOn"]).to_not be_nil
+  expect(result["id"]).to_not be_nil
+  expect(result["href"]).to_not be_nil
+  expect(result["props"]).to_not be_nil
+
+  result["props"].each do |property|
+    prop = @template.props.detect{|u| u.name == property['name']}
+    expect(prop.name).to eql prop.name
+    expect(prop.required).to eql prop.required
+    expect(prop.type).to eql prop.type
+  end
 end
