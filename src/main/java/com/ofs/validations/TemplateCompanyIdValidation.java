277eb8bce9fc0e107efe265d8d800d4af98fe1a0
mpenna <tworker@onyx.ove.com>
2017-06-29 13:13:51 -0400

    Completed getTemplateByCompanyId endpoint

---
+++ src/main/java/com/ofs/validations/TemplateCompanyIdValidation.java
@@ -0,0 +1,7 @@
+package com.ofs.validations;
+
+/**
+ * Created by mpenna on 6/29/17.
+ */
+public interface TemplateCompanyIdValidation {
+}
---
+++ src/main/java/com/ofs/validators/template/TemplateCompanyIdValidator.java
@@ -0,0 +1,7 @@
+package com.ofs.validators.template;
+
+/**
+ * Created by mpenna on 6/29/17.
+ */
+public class TemplateCompanyIdValidator {
+}
