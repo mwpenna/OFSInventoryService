739e1c13232d4289a3d8f5b2fa015e51f9d95177
mpenna <tworker@onyx.ove.com>
2017-07-07 16:28:59 -0400

    Added step defs for missing required prop field

--- automation/features/step_definitions/create_inventory.rb
+++ automation/features/step_definitions/create_inventory.rb
@@ -8,9 +8,23 @@ When(/^A request to create an inventory item is received$/) do
   @result = @service_client.post_to_url_with_auth("/inventory", @inventory.create_to_json, "Bearer "+ "123")
 end
 
-When(/^A request to create an inventory item is received without (.*?)/) do |field|
+When(/^A request to create an inventory item is received without (.*?)$/) do |field|
   @inventory = FactoryGirl.build(:inventory)
   body =  @inventory.create_hash
   body.delete(field.to_sym)
+  @result = @service_client.post_to_url_with_auth("/inventory", body.to_json, "Bearer "+ "123")
+end
+
+When(/^A request to create an inventory item is received with missing prop (.*?)$/) do |field|
+  props = []
+  @template.props.each do |property|
+    prop = FactoryGirl.build(:prop, name: property.name, value: "1234")
+    props << prop
+  end
+  @inventory = FactoryGirl.build(:inventory, companyId: @companyId, props: props, type: @template.name)
+  body = @inventory.create_hash
+  body[:props].each do |prop|
+    prop.delete(field.to_sym)
+  end
   @result = @service_client.post_to_url_with_auth("/inventory", body.to_json, "Bearer "+ "123")
 end
