7d9aacf8b187a565bb0757927274dd113429c64d
mpenna <tworker@onyx.ove.com>
2017-07-07 16:17:03 -0400

    Added missing field automation steps

--- automation/features/step_definitions/create_inventory.rb
+++ automation/features/step_definitions/create_inventory.rb
@@ -1,5 +1,4 @@
 When(/^A request to create an inventory item is received$/) do
-  sleep(1)
   props = []
   @template.props.each do |property|
     prop = FactoryGirl.build(:prop, name: property.name, value: "1234")
@@ -7,4 +6,11 @@ When(/^A request to create an inventory item is received$/) do
   end
   @inventory = FactoryGirl.build(:inventory, companyId: @companyId, props: props, type: @template.name)
   @result = @service_client.post_to_url_with_auth("/inventory", @inventory.create_to_json, "Bearer "+ "123")
+end
+
+When(/^A request to create an inventory item is received without (.*?)/) do |field|
+  @inventory = FactoryGirl.build(:inventory)
+  body =  @inventory.create_hash
+  body.delete(field.to_sym)
+  @result = @service_client.post_to_url_with_auth("/inventory", body.to_json, "Bearer "+ "123")
 end
