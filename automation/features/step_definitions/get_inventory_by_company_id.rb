d3f77617abff33ee8cab37eed6c644c40ba894b8
mpenna <tworker@onyx.ove.com>
2017-07-11 12:57:33 -0400

    Started get by company id automation tests

--- automation/features/step_definitions/get_inventory_by_company_id.rb
+++ automation/features/step_definitions/get_inventory_by_company_id.rb
@@ -1,19 +1,47 @@
 Given(/^A (.*?) user exists and inventory exists for a company$/) do |role|
   @companyId = SecureRandom.uuid
   JWTSubject.new().generate_and_create_jwt_subject({role: 'ADMIN', companyId: @companyId})
-
   @template = Template.new().generate_and_create_template({companyId: @companyId})
-  binding.pry
-
-  props = []
-  @template.props.each do |property|
-    prop = FactoryGirl.build(:prop, name: property.name, required: property.required, type: property.type, value: "1234")
-    props << prop
-  end
-  @inventory = FactoryGirl.build(:inventory, companyId: @companyId, props: props, type: @template.name)
   sleep(0.1)
-  result = @service_client.post_to_url_with_auth("/inventory", @inventory.create_to_json, "Bearer "+ "123")
-  @location = result.headers['location']
-
+  
+  @inventory = []
+  rand(2..5).times do
+    inventory = Inventory.new().generate_and_create_inventory({template: @template, companyId: @companyId,
+                                                               createProps: true, name: Faker::Name.name});
+    @inventory << inventory
+  end
+  sleep(0.1)
   JWTSubject.new().generate_and_create_jwt_subject({role: role, companyId: @companyId})
+end
+
+When(/^A request to get the inventory by company id is received$/) do
+  @result = @service_client.get_by_url_with_auth(@service_client.get_base_uri + '/inventory/company/id/' + @companyId, "Bearer 123")
+end
+
+And(/^I should see the inventory list was returned$/) do
+  expect(@result["count"]).to eql @inventory.size
+  expect(@result["items"].size).to eql @inventory.size
+
+  @result["items"].each do |response|
+    inventory = @inventory.detect{|i| i.id == response['id']}
+
+    expect(response["id"]).to eql inventory.id
+    expect(response["href"]).to eql inventory.href
+    expect(response["createdOn"]).to_not be_nil
+    expect(response["type"]).to eql inventory.type
+    expect(response["companyId"]).to eql @companyId
+    expect(response["price"]).to eql inventory.price
+    expect(response["quantity"]).to eql inventory.quantity
+    expect(response["name"]).to eql inventory.name
+    expect(response["description"]).to eql inventory.description
+    expect(response["props"]).to_not be_nil
+
+    response["props"].each do |property|
+      prop = inventory.props.detect{|u| u.name == property['name']}
+      expect(property["name"]).to eql prop.name
+      expect(property["required"]).to eql prop.required
+      expect(property["type"]).to eql prop.type
+      expect(property["value"]).to eql prop.value
+    end
+  end
 end
--- automation/features/support/models/inventory.rb
+++ automation/features/support/models/inventory.rb
@@ -2,6 +2,32 @@ class Inventory
   attr_accessor  :id, :href, :createdOn, :type, :props,
                  :companyId, :price, :quantity, :name, :description
 
+  def generate_and_create_inventory(args={})
+    default_values(args)
+
+    if(args[:createProps])
+      args[:template].props.each do |property|
+        prop = FactoryGirl.build(:prop, name: property.name, required: property.required, type: property.type, value: "1234")
+        @props << prop
+      end
+    end
+
+    service_client = ServiceClient.new()
+    result = service_client.post_to_url_with_auth("/inventory", create_to_json, "Bearer "+ "123")
+    @href = result.headers['location']
+    @id = @href.split("/id/").last
+    self
+  end
+
+  def default_values(args={})
+    @props = []
+    @type = args[:template].name || "type"
+    @price = args[:price] || 20.50
+    @quantity = args[:quantity] || 100
+    @name = args[:name] || "name"
+    @description = args[:description] ||"description"
+  end
+
   def create_to_json
     {
         type: self.type,
