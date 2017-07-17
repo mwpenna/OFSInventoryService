54f9c274888ac7e46be791e13bfb5dc8d0acdd49
mpenna <tworker@onyx.ove.com>
2017-07-17 14:09:31 -0400

    Completed java code for inventory validate prop value

--- src/main/java/com/ofs/validations/inventory/ValidateInventoryProps.java
+++ src/main/java/com/ofs/validations/inventory/ValidateInventoryProps.java
@@ -4,24 +4,36 @@ import com.ofs.model.Inventory;
 import com.ofs.model.Props;
 import com.ofs.model.Template;
 import com.ofs.repository.TemplateRepository;
+import com.ofs.server.errors.ServerException;
 import com.ofs.server.form.update.ChangeSet;
 import com.ofs.server.model.OFSErrors;
 import com.ofs.validations.InventoryCreateValidation;
 import com.ofs.validations.InventoryUpdateValidation;
+import com.ofs.validations.props.ValidatePropValue;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class ValidateInventoryProps implements InventoryCreateValidation, InventoryUpdateValidation {
 
     @Autowired
     private TemplateRepository templateRepository;
 
+    @Autowired
+    private ValidatePropValue validatePropValue;
+
     @Override
     public void validate(Inventory inventory, OFSErrors errors) throws Exception {
         Optional<Template> templateOptional = templateRepository.getTemplateByName(inventory.getType(), inventory.getCompanyId());
+
+        inventory.getProps().forEach(props -> {
+            validatePropValue(props, errors);
+        });
 
         if(templateOptional.isPresent()) {
             Template template = templateOptional.get();
@@ -58,5 +70,14 @@ public class ValidateInventoryProps implements InventoryCreateValidation, Invent
     private void defaultSystemProps(Props inventoryProp, Props templateProp) {
         inventoryProp.setRequired(templateProp.isRequired());
         inventoryProp.setType(templateProp.getType());
+    }
+
+    private void validatePropValue(Props props, OFSErrors errors) {
+        try {
+            validatePropValue.validate(props, errors);
+        } catch (Exception e) {
+            log.error("Unhandled exception occurred");
+            throw new ServerException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
